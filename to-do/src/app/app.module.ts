6a512304ec0c1af1cc24665a9e7e3a5cd1d7230d
Aleks Bujko <aleksander.bujko@unterschiedundmacher.com>
2021-03-04 12:04:39 +0100

    created basic components

--- to-do/src/app/app.module.ts
+++ to-do/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskInputComponent } from './task-input/task-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TasksComponent,
+    TaskInputComponent
   ],
   imports: [
     BrowserModule
