5e5205b567f273d173b85b82827102c2fe0d68bc
Aleks Bujko <aleksander.bujko@unterschiedundmacher.com>
2021-03-16 16:58:03 +0100

    added mobile styles

--- to-do/src/app/dark-mode.service.ts
+++ to-do/src/app/dark-mode.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DarkModeService {
-  
+
   get theme(): any {
     return document.documentElement.getAttribute('theme');
   }
